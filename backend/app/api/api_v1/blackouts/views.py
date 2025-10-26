8d0d93aead2861714781a655219014731377a506
Mikhail Mamontov <mmontovmm@gmail.com>
2025-10-26 23:29:46 +1000

    delete trash

--- backend/app/api/api_v1/blackouts/views.py
+++ backend/app/api/api_v1/blackouts/views.py
@@ -21,21 +21,3 @@ async def get_blackouts(
     return blackouts
     
 
-# @router.get('', response_model=list[UserRead])
-# async def get_users(
-#     # session: AsyncSession = Depends(db_helper.session_getter),
-#     session: Annotated[AsyncSession, Depends(db_helper.async_session_getter)]
-    
-# ) -> list[User]:
-#     users = await users_crud.get_all_users(session=session) 
-#     return users
-
-
-# @router.post('', response_model=UserRead)
-# async def create_user(
-#     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-#     user_create: UserCreate,
-# ) -> User:
-#     user = await  users_crud.create_user(session=session, user_create=user_create)
-#     return user
-    

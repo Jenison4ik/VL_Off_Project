a2fe010f846a7509af514f9de015d91bfa199358
Evegeniy <109351108+Jenison4ik@users.noreply.github.com>
2025-10-29 17:29:25 +1000

    Улучшена функция получения отключений по ID: добавлена поддержка HTTPS, улучшено управление кэшированием с обновлением каждые 60 секунд, добавлена обработка редиректов при запросе данных.

--- frontend/src/services/getBlackoutsByID.ts
+++ frontend/src/services/getBlackoutsByID.ts
@@ -2,10 +2,22 @@ import { BlackoutByID } from "@/types/Blackout";
 import getServerUrl from "@/services/getServerUrl";
 import { cache } from "react";
 
-//cache, потому что вызываем и для странмцы и для metadata см. https://nextjs.org/docs/app/getting-started/metadata-and-og-images#streaming-metadata
+//cache, чтобы использовать и для страницы, и для metadata
 export default cache(async (id: string): Promise<BlackoutByID> => {
   const { baseUrl, isServer } = getServerUrl();
-  const data = await fetch(baseUrl + "/api/v1/blackouts/" + id);
+
+  // Всегда используем HTTPS, если сервер поддерживает
+  const protocol = baseUrl.startsWith("http://") ? "https://" : "";
+
+  const url = `${protocol}${baseUrl}/api/v1/blackouts/${id}`;
+
+  const data = await fetch(url, {
+    // на сервере Next.js fetch умеет следовать редиректам
+    redirect: "follow",
+    // контроль кэширования
+    next: { revalidate: 60 }, // обновлять кэш каждые 60 секунд
+  });
+
   if (data.status === 404) {
     throw new Error("NOT_FOUND");
   }
@@ -13,5 +25,6 @@ export default cache(async (id: string): Promise<BlackoutByID> => {
   if (!data.ok) {
     throw new Error(`Ошибка сервера: ${data.status}`);
   }
+
   return await data.json();
 });

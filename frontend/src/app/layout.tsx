127470585393dd050d4104c33face183eeea2c63
Evegeniy <109351108+Jenison4ik@users.noreply.github.com>
2025-10-29 17:15:25 +1000

    Обновлён пакет sass до версии 1.93.2, добавлены новые зависимости в package.json и package-lock.json. Удалён файл globals.css и заменён на globals.scss в компоненте layout. Добавлены метаданные для страницы отключений, улучшена обработка ошибок в getBlackoutsByID. Добавлены стили для кнопки полноэкранного режима в компоненте YMap.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import { initMocksServer } from "@/mocks/server";
 
 const geistSans = Geist({
@@ -29,7 +29,7 @@ export default function RootLayout({
         <header>
           <a href="/">Главная</a>
         </header>
-        {children}
+        <div className="content">{children}</div>
       </body>
     </html>
   );
--- frontend/src/services/getBlackoutsByID.ts
+++ frontend/src/services/getBlackoutsByID.ts
@@ -1,9 +1,9 @@
 import { BlackoutByID } from "@/types/Blackout";
 import getServerUrl from "@/services/getServerUrl";
+import { cache } from "react";
 
-export default async function getBlackoutsByID(
-  id: string
-): Promise<BlackoutByID> {
+//cache, потому что вызываем и для странмцы и для metadata см. https://nextjs.org/docs/app/getting-started/metadata-and-og-images#streaming-metadata
+export default cache(async (id: string): Promise<BlackoutByID> => {
   const { baseUrl, isServer } = getServerUrl();
   const data = await fetch(baseUrl + "/api/v1/blackouts/" + id);
   if (data.status === 404) {
@@ -14,4 +14,4 @@ export default async function getBlackoutsByID(
     throw new Error(`Ошибка сервера: ${data.status}`);
   }
   return await data.json();
-}
+});

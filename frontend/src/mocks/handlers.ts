470e676f0b66ba07339234e44bda7012f9827236
Jenison4ik <superjoni121@gmail.com>
2025-10-30 00:55:38 +1000

    Удалены лишние моки

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,11 +1,7 @@
 import { delay, http, HttpResponse } from "msw";
-import { adresses } from "./data/adresses";
 import blackouts from "./data/blackouts.json";
 import blackoutByID from "./data/BlackoutByID.json";
 export const handlers = [
-  http.get("/users", () => {
-    return HttpResponse.json(adresses);
-  }),
   http.get("/api/v1/blackouts", async () => {
     await delay(2000); //Имитация загрузки
     return HttpResponse.json(blackouts);
